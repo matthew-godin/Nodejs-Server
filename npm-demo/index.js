b2f1c95cf9bea65738c3a6d2cd99b9afdecd2f89
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2020-05-11 13:57:38 -0400

    before express

--- npm-demo/index.js
+++ npm-demo/index.js
@@ -1,8 +1,16 @@
-var _ = require('underscore');
+//var _ = require('underscore');
 // require function assumptions
 // 1. Core module
 // 2. File or folder
 // 3. node_modules
-var result = _.contains([1,2,3],2);
+//var result = _.contains([1,2,3],2);
+//console.log(result);
+// Concerning package.json
+// ^ is a caret
+// Major.Minor.Patch
+// ^5.9.13 is like 5.x
+// ~5.9.13 is like 5.9.x (using the latest x)
+// 5.9.13 requires that exact version
+var lion = require('lion-lib-382746');
+var result = lion.add(1, 2);
 console.log(result);
-

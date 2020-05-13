f4afb2d9ecc59fa3ece14611aafdd35083b6b375
Matthew Godin <matthew.godin.software.engineer@gmail.com>
2020-05-13 17:19:48 -0400

    vidly express advanced

--- vidly/index.js
+++ vidly/index.js
@@ -1,55 +1,10 @@
 const Joi = require('joi');
 const express = require('express');
 const app = express();
+const genres = require('./routes/genres');
 app.use(express.json()); // to enable JSON parsing
                          // in Express
-const genres = [];
-app.get('/api/genres', (req, res) => {
-    res.send(genres);
-});
-app.get('/api/genres/:id', (req, res) => {
-    const genre = genres.find(c => c.id ===
-        parseInt(req.params.id));
-    if (!genre) 
-        return res.status(404).send(
-        'The genre with the '
-        + 'given ID was not found');
-    res.send(genre);
-});
-app.post('/api/genres', (req, res) => {
-    const { error } = validateGenre(req.body);
-    if (error) return res.status(400).send(
-            error.details[0].message);
-    const genre = {
-        id: genres.length + 1,
-        name: req.body.name // must enable JSON parsing
-                            // in Express
-    };
-    genres.push(genre);
-    res.send(genre);
-});
-app.put('/api/genres/:id', (req, res) => {
-    const genre = genres.find(c => c.id ===
-        parseInt(req.params.id));
-    if (!genre) return res.status(404).send(
-            'The genre with the '
-            + 'given ID was not found');
-    const { error } = validateGenre(req.body);
-    if (error) return res.status(400).send(
-            error.details[0].message);
-    genre.name = req.body.name;
-    res.send(genre);
-});
-app.delete('/api/genres/:id', (req, res) => {
-    const genre = genres.find(c => c.id ===
-        parseInt(req.params.id));
-    if (!genre) return res.status(404).send(
-            'The genre with the '
-            + 'given ID was not found');
-    const index = genres.indexOf(genre);
-    genres.splice(index, 1);
-    res.send(genre);
-});
+app.use('/api/genres', genres);
 const port = process.env.PORT || 3000;
 app.listen(port, () =>
     console.log(`Listening on port ${port}...`));
